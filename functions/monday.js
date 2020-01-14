6f9591de97187d66be9f91444ae97ac7e1015b33
Martin Pastore <martinpablopastore@gmail.com>
2020-01-14 09:35:51 -0300

    feat(Monday): implementing response

--- functions/monday.js
+++ functions/monday.js
@@ -1,11 +1,25 @@
+const moment = require('moment');
+
 const monday = (req, res) => {
-    res.send({
-        "response_type": "in_channel",
-        "text": "*BUEN LUNES!* A cargar _les hores chiques_!!",
-        "attachments": [{
-            "text": "https://tarmacio.hubplanner.com/time_sheets"
-        }]
-    })
+    let resp = {}
+    if (moment().isoWeekday() === 1) {
+        resp = {
+            "response_type": "in_channel",
+            "text": "*BUEN LUNES!* A cargar _les hores chiques_!!",
+            "attachments": [{
+                "text": "https://tarmacio.hubplanner.com/time_sheets"
+            }]
+        }
+    } else {
+        resp = {
+            "response_type": "in_channel",
+            "text": "A vos te parece que hoy es Lunes?? Cargaste las horas??",
+            "attachments": [{
+                "text": "https://tarmacio.hubplanner.com/time_sheets"
+            }]
+        }
+    }
+    res.send(resp)
 };
 
 module.exports = monday;
