b5bbdb1eee85f189219f62426922017d5bfe53f7
Martin Pastore <martinpablopastore@gmail.com>
2020-01-14 10:17:20 -0300

    feat(Lunch): add lunch command

---
+++ functions/lunch.js
@@ -0,0 +1,6 @@
+const lunch = (req, res) => {
+    res.send({
+        "response_type": "in_channel",
+        "text": "Chicos los que todavía no fueron a comer, por favor recuerden de organizarse para comer y avisarme con la fecha y el lugar elegido"
+    })
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const monday = require('../functions/monday');
 const morning = require('../functions/morning');
+const lunch = require('../functions/lunch');
 
 const router = express.Router();
 
 router.post('/monday', monday)
 router.post('/morning', morning)
+router.post('/lunch', lunch)
 
 module.exports = router;
