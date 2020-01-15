f039d720bc52ed4ecf82f0f63823ab65012cba3a
Martin Pastore <martinpablopastore@gmail.com>
2020-01-15 10:54:15 -0300

    feat(Coca): coca

--- functions/coca.js
+++ functions/coca.js
@@ -1,7 +1,7 @@
 const coca = (req, res) => {
     res.send({
         "response_type": "in_channel",
-        "text": "*VE VO*",
+        "text": "",
         "attachments": [{
             "image_url": "https://pbs.twimg.com/profile_images/1155260737385877504/BKW_01EP_400x400.jpg"
         }]
